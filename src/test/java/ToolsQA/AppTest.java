d2b7ef0bc79e360a0278a47f275c9e91e74f337d
rmattone <37882922+rmattone@users.noreply.github.com>
2022-04-11 21:31:07 -0300

    Update AppTest.java

--- src/test/java/ToolsQA/AppTest.java
+++ src/test/java/ToolsQA/AppTest.java
@@ -11,19 +11,19 @@ public class AppTest {
 
     @DisplayName("Test get Origem")
     @Test
-    public void testGet() {
+    public void testGetOrigem() {
         assertEquals("Origem", classifica.result(0.0,0.0));
     }
 
     @DisplayName("Test get Q1")
     @Test
-    public void testGet() {
+    public void testGetQ1() {
         assertEquals("Q1", classifica.result(0.1, 0.1));
     }
     
     @DisplayName("Test get Q4")
     @Test
-    public void testGet() {
+    public void testGetQ4() {
         assertEquals("Q4", classifica.result(4.5,-2.2));
     }
 }
